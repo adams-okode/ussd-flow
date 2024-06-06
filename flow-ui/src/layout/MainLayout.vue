bc39a7151321a3cd0aaca81e36aa2d0003028c97
Adams Okode <adamsokode@gmail.com>
2024-06-06 19:56:08 +0300

    Update docs link

--- flow-ui/src/layout/MainLayout.vue
+++ flow-ui/src/layout/MainLayout.vue
@@ -76,10 +76,11 @@ onMounted(() => {
           </a>
           <div class="flex items-center lg:order-2">
             <a
-              href="adams-okode.github.io/ussd-flow/docs"
+              href="docs"
               class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-              >Docs</a
             >
+              Docs
+            </a>
 
             <button
               ref="themeToggleBtn"
